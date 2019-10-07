c64364f64d8490438c62209f672900ec7a4500f8
ShivaanKula <54045442+ShivaanKula@users.noreply.github.com>
2019-10-06 18:28:11 -0700

    Add files via upload

---
+++ DonkAI_Kong.lua
@@ -0,0 +1,435 @@
+backgroundColor = 0x80808080
+borderColor = 0xFF000000
+borderColor2 = 0x00000000
+white= 0xFFFFFFFF
+red = 0xFFFF0000
+yellow = 0XFFFFFF00
+pink = 0xFFFF69B4
+lightblue = 0xFFADD8E6
+BoxRadius = 6
+boxStartX = 10
+boxStartY = 30
+boxSide = 70
+boxScale = 255/boxSide
+marioSpriteSize = 16
+enemySpriteSize = 17
+marioXGrid = 0
+marioYGrid = 0
+controller = {}
+displayText = ""
+StopDoingNothing=0
+LastX=0
+LastY=0
+Filename = "DK-1.state"
+ButtonNames = {
+	"A",
+	"B",
+	"Up",
+	"Down",
+	"Left",
+	"Right",
+}
+Level1={}
+Level1[0]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[1]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[2]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[3]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[4]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[5]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[6]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[7]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[8]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[9]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[10] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[11] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[12] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[13] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[14] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,0,0,0,0}
+Level1[15] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[17] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[18] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[19] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[20] = {0,0,0,0,0,0,0,1,1,1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+Level1[21] = {0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0}
+Level1[22] = {0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[23] = {0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[24] = {0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0}
+Level1[25] = {0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0}
+Level1[26] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,0,0,0,0,0,0}
+Level1[27] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[28] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[29] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[30] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[31] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[32] = {0,0,0,0,0,0,0,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+Level1[33] = {0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[34] = {0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[35] = {0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[36] = {0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[37] = {0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[38] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,0,0,0,0,0}
+Level1[39] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[40] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[41] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[42] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[43] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0}
+Level1[44] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+Level1[45] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[46] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[47] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[48] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+Level1[49] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+
+
+Level1Grid={}
+QMap={}
+
+
+
+
+
+
+EnemyXPositions = {} 
+EnemyYPositions = {} 
+
+savestate.load(Filename)
+
+
+function GetMarioPosition()
+	--Use readbyte because we only read the lower half for position values
+	marioX = memory.read_u8(0x0046)
+	marioY = memory.read_u8(0x0047)
+	if LastX == marioX and LastY == marioY then
+		StopDoingNothing =StopDoingNothing +5
+	else
+		StopDoingNothing =0
+	end
+	LastX=marioX
+	LastY=marioY
+end
+
+function GetEnemyPositions()
+	local xMem = 0x0213
+	local yMem = 0x0214
+	for i=0, 11 do
+		--Use readbyte because we only read the lower half for position values
+		EnemyXPositions[i] = memory.readbyte(xMem)
+		EnemyYPositions[i] = memory.readbyte(yMem)
+		xMem = xMem + 0x0010
+		yMem = yMem + 0x0010
+	end
+end
+
+--General purpose function to draw something in the minimap
+function DrawPosInBox(worldX, worldY, col)
+    local localX = worldX/boxScale + boxStartX
+    local localY = worldY/boxScale + boxStartY
+    gui.drawRectangle(localX, localY, 5, 5, borderColor, col)
+end
+
+function DrawMapInBox(worldX, worldY, col)
+    local localX = worldX/boxScale + boxStartX
+    local localY = worldY/boxScale + boxStartY
+    gui.drawRectangle(localX, localY, 2, 2, borderColor2, col)
+end
+
+
+
+function DrawEnemiesInFOV()
+	local offset = 3
+	local marioCenterX = marioX + offset
+	local marioCenterY = marioY + offset
+
+	local maxToTheRight = marioCenterX + marioSpriteSize/2 + marioSpriteSize
+	local maxToTheLeft = marioCenterX - marioSpriteSize/2 - marioSpriteSize
+	local maxToTheTop = marioCenterY - marioSpriteSize/2 - marioSpriteSize
+	local maxToTheBottom = marioCenterY - marioSpriteSize/2
+
+	for i = 0, 11 do
+		if EnemyXPositions[i] ~= 255 and EnemyYPositions[i] ~= 255 then
+			if EnemyXPositions[i] + offset < maxToTheRight and EnemyXPositions[i] - offset > maxToTheLeft then
+				gui.drawRectangle(EnemyXPositions[i], EnemyYPositions[i], 16, 16, borderColor, red)
+
+				-- if EnemyYPositions[i] > maxToTheTop and EnemyYPositions[i] < maxToTheBottom then
+				-- 	gui.drawRectangle(EnemyXPositions[i], EnemyYPositions[i], 16, 16, borderColor, red)
+				-- end
+			end
+		end
+	end
+end
+
+
+function DrawFOV()
+	local offset = 3
+	--Around Mario
+	gui.drawRectangle(marioX + offset - marioSpriteSize/2, marioY + offset - marioSpriteSize/2, marioSpriteSize, marioSpriteSize, borderColor, backgroundColor)
+	--Bottom right
+	gui.drawRectangle(marioX + offset - marioSpriteSize/2 - marioSpriteSize, marioY + offset - marioSpriteSize/2, marioSpriteSize, marioSpriteSize, borderColor, backgroundColor)
+	--Bottom left
+	gui.drawRectangle(marioX + offset - marioSpriteSize/2 + marioSpriteSize, marioY + offset - marioSpriteSize/2, marioSpriteSize, marioSpriteSize, borderColor, backgroundColor)
+	--Top right
+	gui.drawRectangle(marioX + offset - marioSpriteSize/2 - marioSpriteSize, marioY + offset - marioSpriteSize/2 - marioSpriteSize, marioSpriteSize, marioSpriteSize, borderColor, backgroundColor)
+	--Top center
+	gui.drawRectangle(marioX + offset - marioSpriteSize/2, marioY + offset - marioSpriteSize/2 - marioSpriteSize, marioSpriteSize, marioSpriteSize, borderColor, backgroundColor)
+	--Top left
+	gui.drawRectangle(marioX + offset - marioSpriteSize/2 + marioSpriteSize, marioY + offset - marioSpriteSize/2 - marioSpriteSize, marioSpriteSize, marioSpriteSize, borderColor, backgroundColor)
+end
+
+
+
+function DrawMinimap()
+	--Background
+	gui.drawRectangle(boxStartX, boxStartY, boxSide, boxSide,borderColor, backgroundColor)
+	
+	--Hardcoded Map
+	for i=0, 49 do
+		for j=0, 49 do
+			if Level1[i][j] == 1 then
+				DrawMapInBox(j*5,i*5, pink)
+			end
+			if Level1[i][j] == 2 then
+				DrawMapInBox(j*5,i*5, lightblue)
+			end
+        end
+    end
+	
+	
+	--Characters - Mario
+	DrawPosInBox(marioX, marioY, white)
+	--Characters - Enemies
+	for i=0, 11 do
+        if EnemyXPositions[i] ~= 255 and EnemyYPositions[i] ~= 255 then
+            DrawPosInBox(EnemyXPositions[i], EnemyYPositions[i], red)
+        end
+    end
+end
+
+function DrawGrid()
+	
+	for i=0, 24 do
+		for j=0, 24 do
+				gui.pixelText(j*10,i*10,QMap[i][j])
+				if Level1Grid[i][j] == 0 then
+					gui.drawRectangle(j*10, i*10, 10, 10, white)
+				end
+				if Level1Grid[i][j] == 1 then
+					gui.drawRectangle(j*10, i*10, 10, 10, yellow)
+				end
+				if Level1Grid[i][j] == 2 then
+					gui.drawRectangle(j*10, i*10, 10, 10, red)
+				end
+        end
+    end
+	
+end
+
+
+function DrawGridNoNumber()
+	
+	for i=0, 24 do
+		for j=0, 24 do
+				if Level1Grid[i][j] == 0 then
+					gui.drawRectangle(j*10, i*10, 10, 10, white)
+				end
+				if Level1Grid[i][j] == 1 then
+					gui.drawRectangle(j*10, i*10, 10, 10, yellow,yellow)
+				end
+				if Level1Grid[i][j] == 2 then
+					gui.drawRectangle(j*10, i*10, 10, 10, red,red)
+				end
+        end
+    end
+	
+end
+
+
+
+function ClearGridData()
+	Level1Grid[0]   = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[1]   = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[2]   = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[3]   = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[4]   = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[5]   = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[6]   = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[7]   = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[8]   = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[9]   = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[10]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[11]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[12]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[13]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[14]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[15]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[16]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[17]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[18]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[19]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[20]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[21]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[22]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[23]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	Level1Grid[24]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	
+	QMap[0]   = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[1]   = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[2]   = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[3]   = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[4]   = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[5]   = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[6]   = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[7]   = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[8]   = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[9]   = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[10]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,57, 0, 0, 0, 0,0}
+	QMap[11]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,56, 0, 0, 0,0}
+	QMap[12]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,55, 0, 0, 0,0}
+	QMap[13]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,44, 0, 0, 0, 0, 0, 0, 0,53,54, 0, 0, 0,0}
+	QMap[14]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,43, 0,45,46,47,48,49,50,51,52, 0, 0, 0, 0,0}
+	QMap[15]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,42, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[16]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,41,0, 0, 0, 0, 0, 0, 0,32, 0, 0, 0, 0, 0,0}
+	QMap[17]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,40,39,38,37,36,35,34,33, 0,31, 0, 0, 0, 0,0}
+	QMap[18]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,30, 0, 0, 0, 0,0}
+	QMap[19]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,29, 0, 0, 0, 0, 0,0}
+	QMap[20]  = {10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28, 0,28,27,26,-100,-100}
+	QMap[21]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0}
+	QMap[22]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[23]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+	QMap[24]  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}
+end
+
+
+function ReadBoard()
+	j=math.floor(marioX/10)
+	i=math.floor(marioY/10)
+	Level1Grid[i][j] = 1
+	QMap[i][j] = -10+StopDoingNothing
+	marioXGrid=j
+	marioYGrid=i
+	for k = 0, 11 do
+		if EnemyXPositions[k] ~= 255 and EnemyYPositions[k] ~= 255 then
+			j=math.floor((EnemyXPositions[k]+10)/10)
+			i=math.floor((EnemyYPositions[k])/10)
+			Level1Grid[i][j] = 2
+			QMap[i][j] = -20
+			QMap[i-1][j] = 100
+		
+		end
+	end
+	
+end
+
+function CalculateNextMove(x,y)
+	local j = 0
+	local i = 0
+	maxValue = -10
+	Maxj = 0
+	Maxi = 0
+	for j = -1, 1 do
+		for i = -1, 1 do
+		
+			if y+i >=0 and y+i <=24 and x+j >=0 and x+j <=24 then
+			
+				if QMap[y+i][x+j] > maxValue then
+					maxValue=QMap[y+i][x+j]
+					Maxj=j
+					Maxi=i
+					displayText ="MaxValue =" .. maxValue .. " " .. "NextMove = " .. j .. " " .. i
+				end
+				
+			end
+			
+		end
+	end
+	
+
+	
+	if Maxj ==-1 and Maxi == -1 then
+		doMove("Up-Left")
+	elseif Maxj ==0 and Maxi == -1 then
+		doMove("Up")
+	elseif Maxj ==1 and Maxi == -1 then
+		doMove("Up-Right")
+	elseif Maxj ==-1 and Maxi == 0 then
+		doMove("Left")
+	elseif Maxj ==0 and Maxi == 0 then
+		doMove("Stop")
+	elseif Maxj ==1 and Maxi == 0 then
+		doMove("Right")
+	elseif Maxj ==-1 and Maxi == 1 then
+		doMove("Down-Left")
+	elseif Maxj ==0 and Maxi == 1 then
+		doMove("Down")
+	elseif Maxj ==1 and Maxi == 1 then
+		doMove("Down-Right")
+	end
+	
+end
+
+function doMove(Action)
+
+
+
+	if Action =="Up-Left" then
+	
+		controller["P1 Left"] = true
+		controller["P1 A"] = true
+	elseif Action =="Up" then
+		controller["P1 Up"] = true
+	elseif Action =="Up-Right" then
+	
+		controller["P1 Right"] = true
+		controller["P1 A"] = true
+	elseif Action =="Left" then
+	
+		controller["P1 Left"] = true
+	elseif Action =="Stop" then
+
+		controller["P1 Up"] = true
+	elseif Action =="Right" then
+	
+		controller["P1 Right"] = true
+	elseif Action =="Down-Left" then
+		--Do Nothing
+		
+	elseif Action =="Down" then
+	
+		controller["P1 Down"] = true
+		
+	elseif Action =="Down-Right" then
+		--Do Nothing
+	end
+	
+	joypad.set(controller)
+end
+
+function clearMove()
+controller["P1 Up"] = false
+controller["P1 Down"] = false
+controller["P1 Left"] = false
+controller["P1 Right"] = false
+controller["P1 A"] = false
+controller["P1 B"] = false
+end
+
+
+--Main loop
+while true do
+	GetMarioPosition()
+	GetEnemyPositions()
+	ClearGridData()
+	ReadBoard()
+	--DrawMinimap()
+	--DrawFOV()
+	--DrawEnemiesInFOV()
+	DrawGrid()
+	
+	clearMove()
+	CalculateNextMove(marioXGrid,marioYGrid)
+	
+	--displayText ="MarioX Coordinate =" .. marioXGrid .. " " .. "MarioY Coordinate =" .. marioYGrid
+	gui.text(150,100,displayText,white)
+	--DrawGridNoNumber()
+	emu.frameadvance()
+end
